8ab98ef19906d2026209ef5b89211ef5ed08dc59
kokutoupan <kokutou2004@gmail.com>
2025-08-28 16:28:35 +0900

    UIの更新

--- dist/assets/content.ts-DOG3UK7S.js
+++
@@ -1,2 +0,0 @@
-(function(){chrome.runtime.onMessage.addListener(function(n,e,o){return n.action&&(console.log("Action received: "+n.action),window.location.href=n.action,o({result:n.action+" completed"})),!0});
-})()
